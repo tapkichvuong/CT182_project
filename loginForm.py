b48997c770506315d91e5794751295517299644a
Truong Minh Huy <huytruongminh44@gmail.com>
2023-11-03 15:00:47 +0700

    Design Login Form

--- loginForm.py
+++ loginForm.py
@@ -1,35 +1,19 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QDialog, QPushButton, QLabel, QLineEdit, QGridLayout, QMessageBox)
+from PyQt5.QtWidgets import (QApplication, QDialog, QMessageBox)
 from PyQt5.QtCore import QFile, QTextStream
 from main import MainWindow
-
+from Ui_LoginWindow import Ui_Form
 class LoginForm(QDialog):
     def __init__(self):
-        super().__init__()
-        self.setWindowTitle('Library')
-        layout = QGridLayout()
-        label_name = QLabel('<font size="4"> Username </font>')
-        self.lineEdit_username = QLineEdit()
-        self.lineEdit_username.setPlaceholderText('Please enter your username')
-        layout.addWidget(label_name, 0, 0)
-        layout.addWidget(self.lineEdit_username, 0, 1)
-        
-        label_password = QLabel('<font size="4"> Password </font>')
-        self.lineEdit_password = QLineEdit()
-        self.lineEdit_password.setPlaceholderText('Please enter your password')
-        layout.addWidget(label_password, 1, 0)
-        layout.addWidget(self.lineEdit_password, 1, 1)
-        
-        button_login = QPushButton('Login')
-        button_login.clicked.connect(self.handleLogin)
-        layout.addWidget(button_login, 2, 0, 1, 2)
-        layout.setRowMinimumHeight(2, 75)
-        self.setLayout(layout)
-        self.show()
+        super(LoginForm, self).__init__()
+
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.ui.LoginBtn.clicked.connect(self.handleLogin)
         
     def handleLogin(self):
         msg = QMessageBox()
-        if self.lineEdit_username.text() == '1' and self.lineEdit_password.text() == '1':
+        if self.ui.lineEdit_name.text() == '1' and self.ui.lineEdit_pass.text() == '1':
             self.accept()
         else:
             msg.setText('Incorrect Password')
